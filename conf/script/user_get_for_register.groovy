edf9a18dd0cab41f98b4b90ea937f5bf7f7cf489
Hung Luyen <hunglmse@gmail.com>
2019-08-20 21:34:20 +0700

    user_get_for_register

---
+++ conf/script/user_get_for_register.groovy
@@ -0,0 +1,8 @@
+import org.d.api.*;
+
+List<Map<String,String>> thisUser = Main.utility.qry("select * from users where user_id=? ", [user_id], "default");
+if (0 == thisUser.size()) {
+    return "No user with this user_id!!!"
+} else {
+    return thisUser
+}
