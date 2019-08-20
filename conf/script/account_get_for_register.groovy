e33bd8ad3616d9ae8163a311a05edad843bcf2eb
Hung Luyen <hunglmse@gmail.com>
2019-08-20 22:05:36 +0700

    account_get_for_register email null

--- conf/script/account_get_for_register.groovy
+++ conf/script/account_get_for_register.groovy
@@ -1,6 +1,6 @@
 import org.d.api.*;
 
-List<Map<String,String>> thisUser = Main.utility.qry("select * from users where user_id=? ", [user_id], "default");
+List<Map<String,String>> thisUser = Main.utility.qry("select * from users where user_id=? and email is null", [user_id], "default");
 if (0 == thisUser.size()) {
     return "No user with this user_id!!!"
 } else {
