90c6cc4a5fc4ca038d947bfdef6bc948753ede1a
Hung Luyen <hunglmse@gmail.com>
2019-08-24 19:49:26 +0700

    user 1st api - remove password

--- conf/script/user_get_for_register.groovy
+++ conf/script/user_get_for_register.groovy
@@ -1,6 +1,6 @@
 import org.d.api.*;
 
-List<Map<String,String>> thisUser = Main.utility.qry("select * from users where user_id=? and email is null", [user_id], "default");
+List<Map<String,String>> thisUser = Main.utility.qry("select user_id,email,telegram_id,phone,first_name,last_name,user_image,username,create_at,update_at,active from users where user_id=? and email is null", [user_id], "default");
 if (0 == thisUser.size()) {
     return ["error":"No user with this user_id or user has registered!!!"]
 } else {
