c845e167a51ef34d3a2682bb894726e8413bfeef
Hung Luyen <hunglmse@gmail.com>
2019-08-24 20:56:54 +0700

    user 1st api - add http_code

--- conf/script/user_get_for_register.groovy
+++ conf/script/user_get_for_register.groovy
@@ -2,7 +2,7 @@ import org.d.api.*;
 
 List<Map<String,String>> thisUser = Main.utility.qry("select user_id,email,telegram_id,phone,first_name,last_name,user_image,username,create_at,update_at,active from users where user_id=? and email is null", [user_id], "default");
 if (0 == thisUser.size()) {
-    return ["error":"No user with this user_id or user has registered!!!"]
+    return ["http_code":"404","error":"No user with this user_id or user has registered!!!"]
 } else {
-    return thisUser.get(0)
+    return ["http_code":"202","result":thisUser.get(0)]
 }
